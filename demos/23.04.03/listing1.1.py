fb32e240473becc4ccbd75808b79f6df77b62418
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-04-05 13:26:51 -0700

    Modify via live demo

--- demos/23.04.03/listing1.1.py
+++ demos/23.04.03/listing1.1.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """Calculate the area and circumference of a circle from its radius."""
-import math
+import math  # statement
 
-radius = int(input("Enter the radius of your circle: "))
+user_input = input("Enter the radius of your circle: ")  # expression
+radius = int(user_input)
+
+# radius = int(input("Enter the radius of your circle: "))  # expression
 
 circumference = 2 * math.pi * radius
 area = math.pi * radius**2
 
-print(f"The circumference of your circle is {circumference:0.3f}, and the area is {area:0.3f}")
+print(
+    f"The circumference of your circle is {circumference:0.3f}, and the area is {area:0.3f}"
+)  # statement
