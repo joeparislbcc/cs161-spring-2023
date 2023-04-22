335a1573a33d7cd4c6bd0a6e63589244aa17a29f
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-04-22 13:11:59 -0700

    Initial commit

---
+++ demos/23.04.26/demo5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Read a particular line from a text file.
+
+Both the name of the text file and the number of the line to read from it are
+specified by the user."""
+
+
+def main():
+    filename = input("Please enter the name of the file to read from: ")
+    target_line_number = int(input("Please enter the line number to read: "))
+
+    try:
+        with open(filename, mode="r", encoding="utf-8") as infile:
+            target_line_number = int(target_line_number)
+            line_count = 1
+
+            for line in infile:
+                if line_count == target_line_number:
+                    print(f'Line {target_line_number} of file {filename} is "{line}"')
+                    break
+                line_count += 1
+            else:  # for...else
+                print(f"Line {target_line_number} of file {filename} does not exist")
+
+    except FileNotFoundError:
+        print(f"The file {filename} does not exist")
+
+    except ValueError:
+        print(f"{target_line_number} is not a legal line number")
+
+
+if __name__ == "__main__":
+    main()
---
+++ demos/23.04.26/demo6.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Read a particular line from a text file.
+
+Both the name of the text file and the number of the line to read from it are
+specified by the user."""
+import sys
+
+
+def main():  # McCabe strikes again!!
+    while True:
+        try:
+            filename = input("Please enter the name of the file to read from: ")
+        except EOFError:
+            sys.exit()
+
+        try:
+            with open(filename, mode="r", encoding="utf=8") as infile:
+                lines = infile.readlines()
+        except FileNotFoundError:
+            print(f"{filename}: File not found")
+        else:
+            break
+
+    while True:
+        try:
+            target_line_number = int(input("Please enter the line number to read: "))
+        except ValueError:
+            print("Invalid line number. Please try again.")
+        else:
+            break
+
+    try:
+        print(f'Line {target_line_number} is "{lines[target_line_number - 1]}"')
+    except IndexError:
+        print(f"Line {target_line_number} does not exist in file {filename}")
+
+
+if __name__ == "__main__":
+    main()
