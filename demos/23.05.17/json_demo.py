3adff7071d4df03d0b32c60d30343d0f0383dbfd
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-05-20 13:30:56 -0700

    Get users

--- demos/23.05.17/json_demo.py
+++ demos/23.05.17/json_demo.py
@@ -5,13 +5,18 @@ import json
 
 import requests  # pip install requests
 
-response = requests.get("https://jsonplaceholder.typicode.com/todos/37", timeout=1)
+response = requests.get("https://jsonplaceholder.typicode.com/users", timeout=1)
 
 if response.ok:
-    users = response.json()
+    # users = response.json()
+    users = json.dumps(response.json(), indent=2)
 
     # print(users)
-    print(json.dumps(users, indent=2))
+    # print(json.dumps(users, indent=2))
+    print(users)
     # print(json.dumps(users[0], indent=2))
     # print(type(users[0]["completed"]))
     # print(users[0].keys())
+
+    with open("users.json", mode="w", encoding="utf-8") as outfile:
+        outfile.write(users)
